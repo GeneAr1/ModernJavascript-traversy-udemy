b42a4580010532bbba0142a160bb94950562fa88
Gene Arsenault <geneaar@outlook.com>
2018-03-29 16:21:45 -0400

    commit of training files

---
+++ 2-14-project-files/2_14_project_files/js_sandbox/app.js
@@ -0,0 +1,50 @@
+// FUNCTION DECLARATIONS
+
+function greet(firstName = 'John', lastName = 'Doe'){
+  // if(typeof firstName === 'undefined'){firstName = 'John'}
+  // if(typeof lastName === 'undefined'){lastName = 'Doe'}
+  //console.log('Hello');
+  return 'Hello ' + firstName + ' ' + lastName;
+}
+
+// console.log(greet());
+
+// FUNCTION EXPRESIONS
+
+const square = function(x = 3){
+  return x*x;
+};
+
+// console.log(square());
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+//77
+// these can be very useful expecially in some design patterns according to brad
+// will be used in module pattern later
+
+// (function(){
+//   console.log('IIFE Ran..');
+// })();
+
+// (function(name){
+//   console.log('Hello '+ name);
+// })('Brad');
+
+// PROPERTY METHODS  !! when function put inside object it is called a method
+
+const todo = {
+  add: function(){
+    console.log('Add todo..');
+  },
+  edit: function(id){
+    console.log(`Edit todo ${id}`);
+  }
+}
+
+todo.delete = function(){
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
